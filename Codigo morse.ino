c3267fd03719961a7c6b01d9333ea355696bb196
agustintemoiko <agustin.temoiko.edu.ar>
2021-07-01 12:09:57 -0300

    morse

---
+++ Codigo morse.ino
@@ -0,0 +1,78 @@
+int led1=1;
+int raya=600;
+int punto=300;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led1, OUTPUT);
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(raya);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(600);    
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(raya);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(raya);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(600);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(raya);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(600);  
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);  
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100); 
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(600);  
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(raya);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(600);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(600);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(raya);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(punto);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(600);
+      
+      
+}
